0a846b65bb400fe07405bc1783a1a2ff1bb71790
David White <dwhite0627@gmail.com>
2020-04-19 18:20:20 -0400

    Add car routes to server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const morgan = require("morgan");
 // Middlewarae
 server.use(express.json());
 
-// Custom middleware
+// Routes
+const carsRoute = require("./cars/carRouter");
+server.use("/cars", carsRoute);
 
 /* Third party middleware */
 server.use(cors());
@@ -15,7 +17,7 @@ server.use(morgan("dev"));
 /* Error middleware */
 server.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ message: 'There was an error performing the required operation' })
+  res.status(500).json({ message: "There was an error performing the required operation" })
 });
 
 module.exports = server;
