7c1ba9005e9ceb6604417719428fec7d4c1457d0
David White <dwhite0627@gmail.com>
2020-04-19 18:19:10 -0400

    Add get and getById db helpers

--- cars/carDb.js
+++ cars/carDb.js
@@ -16,3 +16,19 @@ function insert(car) {
       return getById(ids[0]);
     });
 }
+
+// Read
+function get(){
+  return db("cars");
+}
+
+/**
+ * @function getById: Get the data for the resource with id
+ * @param {*} id: The id of the resource to fetch
+ * @returns: none
+ */
+function getById(id) {
+  return db("cars")
+    .where({ id })
+    .first();
+}
