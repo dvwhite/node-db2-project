f1cc5f3a2d2ebc3bcfd4120705360e764a92508b
David White <dwhite0627@gmail.com>
2020-04-20 13:41:10 -0400

    Add errDetails middleware, refactor catch blocks, add validateCarVIN

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -2,23 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // Database helper functions
-const {
-  get,
-  getById,
-  insert,
-  update,
-  remove
-} = require("./carDb");
+const { get, getById, getByVIN, insert, update, remove } = require("./carDb");
+
+// Constants
+VIN_LENGTH = 17;
 
 // Add a new car record
-router.post("/", async (req, res, next) => {
+router.post("/", validateCarVIN, async (req, res, next) => {
   try {
-    const newCar = insert(req.body);
+    const newCar = await insert(req.body);
     res.status(200).json(newCar);
-  }
-  catch (err) {
-    console.log(err);
-    next(err);
+  } catch (err) {
+    errDetail(res, err);
   }
 });
 
@@ -27,35 +22,29 @@ router.get("/", async (req, res, next) => {
   try {
     const cars = await get();
     res.status(200).json(cars);
-  }
-  catch (err) {
-    console.log(err);
-    next(err);
+  } catch (err) {
+    errDetail(res, err);
   }
 });
 
 // Get a specific car by id
 router.get("/:id", validateCarId, async (req, res, next) => {
   try {
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
     const car = await getById(id);
     res.status(200).json(car);
-  }
-  catch (err) {
-    console.log(err);
-    next(err);
+  } catch (err) {
+    errDetail(res, err);
   }
 });
 
 // Update a car by id
-router.put("/:id", validateCarId, async (req, res, next) => {
+router.put("/:id", validateCarId, validateCarVIN, async (req, res, next) => {
   try {
     const newCar = await insert(req.body);
     res.status(200).json(newCar);
-  }
-  catch {
-    console.log(err);
-    next(err);
+  } catch (err) {
+    errDetail(res, err);
   }
 });
 
@@ -64,12 +53,10 @@ router.delete("/:id", validateCarId, async (req, res, next) => {
   try {
     await remove(id);
     res.send(204).end();
+  } catch (err) {
+    errDetail(res, err);
   }
-  catch {
-    console.error(err);
-    next(err);
-  }
-})
+});
 
 /**
  * @function validateCarId: Validate the the id exists before submitting req
@@ -83,14 +70,55 @@ async function validateCarId(req, res, next) {
     const id = Number(req.params.id);
     const car = await getById(id);
     if (!car) {
-      return res.status(404).json({ message: "User id doesn't exist"})
+      return res.status(404).json({
+        message: "Not Found",
+        validation: ["User id doesn't exist"],
+        data: {},
+      });
     }
     next();
+  } catch (err) {
+    errDetail(res, err);
   }
-  catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "There was an error performing the required operation" });
+}
+
+/**
+ * @function validateCarVIN: Validate the the VIN is unique before submitting req
+ * @param {*} req: The request object sent to the API
+ * @param {*} res: The response object sent from the API
+ * @param {*} next: The express middleware function to move to the next middleware
+ * @returns: none
+ */
+async function validateCarVIN(req, res, next) {
+  try {
+    const VIN = req.body.VIN;
+    const car = await getByVIN(VIN);
+    if (car) {
+      return res.status(404).json({
+        message: "Bad Request",
+        validation: ["VIN already exists"],
+        data: {},
+      });
+    } else if (VIN.length > VIN_LENGTH) {
+      return res.status(400).json({
+        message: "Bad Request",
+        validation: ["VIN is not the required length"],
+        data: {},
+      });
+    }
+    next();
+  } catch (err) {
+    errDetail(res, err);
   }
+}
+
+function errDetail(res, err) {
+  console.log(err);
+  return res.status(500).json({
+    message: "There was an error performing the required operation",
+    validation: [],
+    data: {},
+  });
 }
 
 module.exports = router;
